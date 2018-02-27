5f2c901599dc15547437ccdc119b9b7b415913f9
krishnasony97@gmail.com <krishnasony97@gmail.com>
2018-02-28 01:13:47 +0530

    this is include databse php page

---
+++ Android/include/DbConnect.php
@@ -0,0 +1,22 @@
+<?php
+  
+   /**
+   * 
+   */
+   class DbConnect 
+   {
+   private	$con;
+   	
+		   
+		   function connect()
+		   {
+		   	include_once dirname(__FILE__).'/Constants.php';
+		   	$this->con = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+            
+            if(mysqli_connect_errno()){
+            	echo "failed to connect with database!".mysqli_connect_errno();
+            }
+            return $this->con;
+		   }
+
+  }
